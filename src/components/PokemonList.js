be87bb609eed88f36090b588bbd54f98e0cffb59
GavDotZip <gavdotzip@gmail.com>
2024-02-04 14:23:40 +0000

    add commit

---
+++ src/components/PokemonList.js
@@ -0,0 +1,14 @@
+// src/components/PokemonList.js
+import React from 'react';
+
+const PokemonList = () => {
+  // Call API for Pokemon Data
+
+  return (
+    <div>
+      {/* Card Format Display */}
+    </div>
+  );
+}
+
+export default PokemonList;
