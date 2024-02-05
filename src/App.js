b3f86eea0ec9bd52e3320af3840472b80cf0edef
GavDotZip <gavdotzip@gmail.com>
2024-02-05 14:02:43 +0000

    style and comments

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
+// Importing logo and styles for the App
 import logo from './logo.svg';
 import './App.css';
+
+// Importing React and necessary components from react-router-dom
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import PokemonList from './components/PokemonList'; // Import the PokemonList component
 
-
+// Main App component
 function App() {
   return (
+    // Using the BrowserRouter to enable routing in the app
     <Router>
       <div>
+        {/* Navigation bar with links to different routes */}
         <nav>
           <ul>
             <li> <Link to="/"> Home</Link></li>
@@ -17,10 +22,16 @@ function App() {
           </ul>
         </nav>
 
+        {/* Horizontal line for visual separation */}
         <hr />
+
+        {/* Defining routes using react-router-dom */}
         <Routes>
+          {/* Route for the home page, rendering the PokemonList component */}
           <Route path="/" element={<PokemonList />} />
         </Routes>
+
+        {/* Additional routes (commented out for now) */}
         {/* <Route path="/story-missions" component={StoryMissions} />
         <Route path="/locations" component={Locations} /> */}
       </div>
@@ -28,4 +39,5 @@ function App() {
   );
 }
 
+// Exporting the App component as the default export
 export default App;
